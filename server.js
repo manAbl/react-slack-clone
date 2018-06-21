db7ae28fe6db182798dc8b1a1a644fc264a172af
manuel blanco <manuel.22.lamas@gmail.com>
2018-06-21 13:49:09 -0400

    Added basic logic and created message screen component

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ChatkitS = require('pusher-chatkit-server');
 
-const app = express()
+const app = express();
+
+const chatkit = new ChatkitS.default({
+  instanceLocator:'v1:us1:8fbd043f-7b88-41e3-816a-6a9eb04360cf',
+  key:'6a6c76ec-1663-4975-bd5d-c1c33688fa3b:CKYtwSi7YqGqr3rlKN9ViEVKUZFALIUnegRQ9jZpSFM='
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+
+app.post('/users', (req,res) => {
+  const { username } = req.body;
+
+  chatkit.createUser({
+    name: username,
+    id: username
+  }).then(() => res.sendStatus(201)).cacth(error => {
+    if( error.error_type === 'services/chatkit/user_already_exists') {
+      res.sendStatus(200)
+    } else {
+      res.status(error.statusCode).json(error)
+    }
+  })
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import UsernameForm from './components/UsernameForm';
+import ChatScreen from './ChatScreen';
 
 class App extends Component {
-  render() {
-    return <h1>Slack</h1>
+  constructor() {
+    super()
+    this.state = {
+      currentScreen: 'WhatIsYourUsernameScreen',
+      currentUsername: '',
+    };
   }
-}
+
+  onUsernameSubmit = () => {
+    fetch('http://localhost:3001/users', {
+      method:'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({username})
+    }).then(response => {
+        this.setState({
+          currentUsername: response,
+          currentScreen: 'ChatScreen'
+        });
+    }).catch( err => {
+      console.error(err);
+    })
+  }
+
+  render() {
+    if(this.state.currentScreen === 'WhatIsYourUsernameScreen' ) {
+          return <UsernameForm onSubmit={this.onUsernameSubmit} />
+    } else {
+      return <ChatScreen username={this.state.currentUsername} />
+    }
+  }
+};
 
 export default App
